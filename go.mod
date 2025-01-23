7f3230bff7d58299794fd6be346a245086f5f07e
oldweipro <oldweipro@gmail.com>
2025-01-23 16:27:51 +0800

    优化代码结构

--- go.mod
+++ go.mod
@@ -3,35 +3,44 @@ module github.com/oldweipro/bilibili-download
 go 1.20
 
 require (
-	github.com/AlecAivazis/survey/v2 v2.3.7
+	github.com/charmbracelet/bubbletea v1.2.4
 	github.com/imroc/req/v3 v3.41.10
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v1.0.0 // indirect
+	github.com/charmbracelet/x/ansi v0.4.5 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/gaukas/godicttls v0.0.4 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/pprof v0.0.0-20230811205829-9131a7e9cc17 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.8 // indirect
-	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/onsi/ginkgo/v2 v2.11.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
 	github.com/quic-go/qtls-go1-20 v0.3.2 // indirect
 	github.com/quic-go/quic-go v0.37.4 // indirect
 	github.com/refraction-networking/utls v1.4.3 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb // indirect
 	golang.org/x/mod v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"os/exec"
+	"strings"
 	"time"
 )
 
-func main() {
-	x()
-}
+var bvId = "BV1pqwFevEQ4"
 
-func x() {
-	hasFfmpeg, err := FfmpegVersion()
+func main() {
+	if bvId == "" {
+		return
+	}
+	err := FfmpegVersion()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -20,19 +26,14 @@ func x() {
 	flag, msg := CheckAccount()
 	fmt.Println(msg)
 	if !flag {
-		isLoginVip := AskIsLoginVip()
-		if isLoginVip {
-			loginScan := QrcodeLoginScan()
-			if loginScan {
-				flag = loginScan
-				fmt.Println("👏 欢迎尊贵的大会员用户！👏")
-			} else {
-				fmt.Println("☹️ 抱歉您不是大会员用户！☹️")
-			}
+		loginScan := QrcodeLoginScan()
+		if loginScan {
+			flag = loginScan
+			fmt.Println("👏 欢迎尊贵的大会员用户！👏")
+		} else {
+			fmt.Println("☹️ 抱歉您不是大会员用户！☹️")
 		}
 	}
-	// 输入视频链接或者BV
-	bvId := AskBV()
 	fmt.Println(bvId)
 	// 保存路径
 	savePath, err := GetSavePath()
@@ -41,7 +42,7 @@ func x() {
 		return
 	}
 	fmt.Println("📁视频存储路径:", savePath)
-	if !hasFfmpeg || !flag {
+	if !flag {
 		videoQuality := AskSelectMp4VideoQuality(bvId)
 		DownloadMp4Video(bvId, savePath, videoQuality)
 		return
@@ -70,3 +71,178 @@ func x() {
 	}
 	fmt.Println("\n✅ 下载完成!")
 }
+
+//============================== bubbletea start ==============================
+
+func AskSelectMp4VideoQuality(bv string) int {
+	data := Mp4VideoPlay(bv, 16)
+	quality := data.Data.AcceptDescription
+	nums := data.Data.AcceptQuality
+
+	m := model{
+		qualityOptions:  quality,
+		selectedQuality: quality[0], // 默认选择第一个
+	}
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("启动程序时出错: %v\n", err)
+		return 16
+	}
+
+	// 返回所选质量对应的数字
+	videoQuality := findIntByQuality(quality, nums, m.selectedQuality)
+	return videoQuality
+}
+
+type model struct {
+	qualityOptions  []string
+	selectedQuality string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+// 处理用户输入
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "up":
+			m.selectedQuality = m.previousQuality()
+		case "down":
+			m.selectedQuality = m.nextQuality()
+		case "enter":
+			fmt.Printf("你选择的视频质量是: %s\n", m.selectedQuality)
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+// 渲染界面
+func (m model) View() string {
+	var sb strings.Builder
+	sb.WriteString("请选择视频清晰度:\n")
+	for _, quality := range m.qualityOptions {
+		if quality == m.selectedQuality {
+			sb.WriteString(fmt.Sprintf("> %s\n", quality))
+		} else {
+			sb.WriteString(fmt.Sprintf("  %s\n", quality))
+		}
+	}
+	sb.WriteString("\n按上下键选择，按回车确认")
+	return sb.String()
+}
+
+// 获取下一个质量
+func (m model) nextQuality() string {
+	for i, quality := range m.qualityOptions {
+		if quality == m.selectedQuality {
+			if i+1 < len(m.qualityOptions) {
+				return m.qualityOptions[i+1]
+			}
+			return m.qualityOptions[0] // 循环回到第一个
+		}
+	}
+	return m.qualityOptions[0] // 默认返回第一个
+}
+
+// 获取上一个质量
+func (m model) previousQuality() string {
+	for i, quality := range m.qualityOptions {
+		if quality == m.selectedQuality {
+			if i-1 >= 0 {
+				return m.qualityOptions[i-1]
+			}
+			return m.qualityOptions[len(m.qualityOptions)-1] // 循环回到最后一个
+		}
+	}
+	return m.qualityOptions[0] // 默认返回第一个
+}
+
+// 根据给定的质量列表和所选质量返回视频质量的数字表示
+func findIntByQuality(quality []string, nums []int, selectedQuality string) int {
+	for i, q := range quality {
+		if q == selectedQuality {
+			return nums[i]
+		}
+	}
+	return nums[0] // 默认返回第一个质量
+}
+func AskSelectVideoQuality(bv string) int {
+	quality, nums := GetVideoQuality(bv)
+
+	m := model{
+		qualityOptions:  quality,
+		selectedQuality: quality[0], // 默认选择第一个
+	}
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("启动程序时出错: %v\n", err)
+		return 16
+	}
+
+	// 返回所选质量对应的数字
+	videoQuality := findIntByQuality(quality, nums, m.selectedQuality)
+	return videoQuality
+}
+
+func AskSelectAudioQuality(bv string) int {
+	quality, nums := GetAudioQuality(bv)
+
+	m := model{
+		qualityOptions:  quality,
+		selectedQuality: quality[0], // 默认选择第一个
+	}
+
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("启动程序时出错: %v\n", err)
+		return 16
+	}
+
+	// 返回所选质量对应的数字
+	videoQuality := findIntByQuality(quality, nums, m.selectedQuality)
+	return videoQuality
+}
+
+//============================== bubbletea end ==============================
+
+//============================== ffmpeg start ==============================
+
+// FfmpegVersion 检查是否安装ffmpeg
+func FfmpegVersion() error {
+	cmd := exec.Command("ffmpeg", "-version")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	if err != nil {
+		return errors.New("未找到ffmpeg, 请先安装")
+	}
+	return nil
+}
+
+// FfmpegMergeFile 使用ffmpeg合并文件
+func FfmpegMergeFile(fileList *[]string, outFile *string) error {
+	var arg []string
+	for _, fp := range *fileList {
+		arg = append(arg, "-i", fp)
+	}
+
+	arg = append(arg, "-vcodec", "copy", "-acodec", "copy", *outFile)
+	cmd := exec.Command("ffmpeg", arg...)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &out
+	err := cmd.Run()
+	if err != nil {
+		return errors.New(fmt.Sprintf("%s: %s", "文件合并失败", out.String()))
+	}
+	return nil
+}
+
+//============================= ffmpeg end ==============================
